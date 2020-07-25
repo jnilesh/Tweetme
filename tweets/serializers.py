7e548ab7f29841fc1fad94855499d1d878f39800
Nilesh Jain <njain.we@gmail.com>
2020-07-25 23:33:20 +0530

    retweet started

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -23,6 +23,8 @@ class TweetSerializer(serializers.ModelSerializer):
 class TweetActionSerializer(serializers.Serializer):
         id = serializers.IntegerField()
         action = serializers.CharField()
+        content = serializers.CharField(allow_blank=True, required=False)
+
 
         def validate_action(self,value):
             value = value.lower().strip()
--- tweets/views.py
+++ tweets/views.py
@@ -65,6 +65,7 @@ def tweet_action_view(request, *args, **kwargs):
         data = serializer.validated_data
         tweet_id = data.get("id")
         action = data.get("action")
+        content = data.get("content")
         qs = Tweet.objects.filter(id=tweet_id)
         if not qs.exists():
             return Response({}, status=404)
@@ -76,7 +77,9 @@ def tweet_action_view(request, *args, **kwargs):
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
-            # this is todo
+            new_tweet = Tweet.objects.create(user=request.user,parent=obj,content=content)
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data, status=200)
             pass
     return Response({}, status=200)
 
