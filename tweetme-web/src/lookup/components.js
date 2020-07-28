ba3406c2cb375bbe3fa1d7f7a3b53897226e56d3
Nilesh Jain <njain.we@gmail.com>
2020-07-28 20:29:11 +0530

    django react join

--- tweetme-web/src/lookup/components.js
+++ tweetme-web/src/lookup/components.js
@@ -1,5 +1,4 @@
 
-
 export function loadTweets(callback) {
     const xhr = new XMLHttpRequest()
     const method = 'GET' // "POST"
@@ -8,14 +7,11 @@ export function loadTweets(callback) {
     xhr.responseType = responseType
     xhr.open(method, url)
     xhr.onload = function() {
-      callback(xhr.response, xhr.status) 
-        
-      
+      callback(xhr.response, xhr.status)
     }
-    xhr.onerror= function(e) {
+    xhr.onerror = function (e) {
       console.log(e)
-      callback({"message":"the request was an error"}, 400)
+      callback({"message": "The request was an error"}, 400)
     }
     xhr.send()
-  
   }
