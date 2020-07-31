4a08ee6d30bdd20f8c6393e7676c29c0b93c7ce6
Nilesh Jain <njain.we@gmail.com>
2020-07-31 18:19:29 +0530

    user following

---
+++ profiles/api/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render , redirect
+from django.conf import settings
+from django.http import HttpResponse, Http404, JsonResponse 
+from django.utils.http import is_safe_url
+from ..models import Profile
+import random
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,permission_classes,authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
+from django.contrib.auth import get_user_model
+# Create your views here.
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
+User = get_user_model()
+
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def user_profile_detail_view(request,username, *args, **kwargs):
+#     current_user = request.user
+#     return Response({}, status=400)
+
+
+@api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
+def user_follow_view(request,username, *args, **kwargs):
+    me = request.user
+    other_user_qs = User.objects.filter(username=username)
+    if me.username == username:
+        my_followers = me.profile.followers.all()
+        return Response({"count":my_followers.count()}, status=200)
+
+    if not other_user_qs.exists():
+        return Response({}, status=404)
+    other = other_user_qs.first()
+    profile = other.profile
+    data = {}
+    try:
+        data = request.data
+    except:
+        pass
+    print(data)
+    action = data.get("action")
+    if action == "follow":
+        profile.followers.add(me)
+    elif action == "unfollow":
+        profile.followers.remove(me)
+    else:
+        pass
+    current_followers_qs = profile.followers.all()
+    return Response({"count":current_followers_qs.count()}, status=200)
+
